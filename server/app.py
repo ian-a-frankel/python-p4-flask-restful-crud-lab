5e8fd50cb8b25412e051fe179a6965103e43b83d
Ian Frankel <ian.frankel@gmail.com>
2023-12-13 09:40:58 -0500

    gracefully added routes

--- server/app.py
+++ server/app.py
@@ -46,6 +46,24 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self,id):
+        plant = Plant.query.filter_by(id=id).first()
+        new_data = request.get_json()
+        for key in new_data:
+            setattr(plant, key, new_data[key])
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 201)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response(jsonify({}),204)
+        else:
+            return make_response(jsonify({'error':'plant not found'}),404) 
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
